5cdd60006f2c1b529781331dd972d1205f6b31cd
llock1 <don882193@gmail.com>
2025-04-01 00:06:26 +0100

    login improvments

--- frontend/src/layouts/DashboardLayout.vue
+++ frontend/src/layouts/DashboardLayout.vue
@@ -40,6 +40,15 @@
             <span class="ms-3">Dashboard</span>
           </router-link>
         </li>
+        <li>
+          <router-link to="/logout" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+              <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+            </svg>
+            <span class="ms-3">Logout</span>
+          </router-link>
+        </li>
       </ul>
     </div>
   </aside>
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -56,20 +56,52 @@ onMounted(
 </script>
 
 <template>
-  <section id="login">
-    <div class="flex justify-center items-center h-[100dvh]">
-      <div class="card border border-2 border-grey-700 bg-grey-800 p-8">
-        <form @submit.prevent="handleLogin" class="flex flex-col gap-4 w-[300px]">
-          <div class="flex flex-col gap-2">
-            <label for="username">Username:</label>
-            <input type="text" v-model="username" name="username" id="username" class="border border-2 border-black">
-          </div>
-          <div class="flex flex-col gap-2">
-            <label for="password">Password:</label>
-            <input type="password" v-model="password" name="password" id="password" class="border border-2 border-black">
-          </div>
-          <input type="submit" value="SUBMIT">
-        </form>
+<!--  <section id="login">-->
+<!--    <div class="flex justify-center items-center h-[100dvh]">-->
+<!--      <div class="card border border-2 border-grey-700 bg-grey-800 p-8">-->
+<!--        <form @submit.prevent="handleLogin" class="flex flex-col gap-4 w-[300px]">-->
+<!--          <div class="flex flex-col gap-2">-->
+<!--            <label for="username">Username:</label>-->
+<!--            <input type="text" v-model="username" name="username" id="username" class="border border-2 border-black">-->
+<!--          </div>-->
+<!--          <div class="flex flex-col gap-2">-->
+<!--            <label for="password">Password:</label>-->
+<!--            <input type="password" v-model="password" name="password" id="password" class="border border-2 border-black">-->
+<!--          </div>-->
+<!--          <input type="submit" value="SUBMIT">-->
+<!--        </form>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </section>-->
+
+  <section class="bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        Licensing System
+      </a>
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Sign in to your account
+          </h1>
+          <form class="space-y-4 md:space-y-6" @submit.prevent="handleLogin">
+            <div>
+              <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
+              <input type="text" v-model="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="">
+            </div>
+            <div>
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+              <input type="password" v-model="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+            </div>
+            <div class="flex items-center justify-center">
+              <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-primary-500">Forgot password?</a>
+            </div>
+            <button type="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   </section>
