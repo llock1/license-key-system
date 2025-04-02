992df46ed1da70fe34262d822b7d5b0ed7c8e42b
llock1 <don882193@gmail.com>
2025-04-02 19:18:38 +0100

    LicenseKey expires at field

--- backend/models/license.go
+++ backend/models/license.go
@@ -2,11 +2,13 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"time"
 	// "github.com/google/uuid"
 )
 
 type LicenseKey struct {
 	gorm.Model
-	Key  string `gorm:"type:uuid"`
-	Hwid string
+	Key       string `gorm:"type:uuid"`
+	Hwid      string
+	ExpiresAt *time.Time
 }
