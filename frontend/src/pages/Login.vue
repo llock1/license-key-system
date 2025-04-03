2156d4b045d5020feaceb0b04cd8e67ef329f498
llock1 <don882193@gmail.com>
2025-04-03 11:29:54 +0100

    change frontend to use email aswell

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -41,7 +41,7 @@ const handleLogin = async () => {
       });
     }
   } catch (error) {
-    toast.error("Your username or password is incorrect!", {
+    toast.error("Your username/email or password is incorrect!", {
       timeout: 2000
     });
   }
@@ -66,8 +66,8 @@ onMounted(() => {
           </h1>
           <form class="space-y-4 md:space-y-6" @submit.prevent="handleLogin" autocomplete="off">
             <div>
-              <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
-              <input type="text" v-model="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="">
+              <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Username or Email</label>
+              <input type="text" v-model="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username/email" required="">
             </div>
             <div>
               <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
